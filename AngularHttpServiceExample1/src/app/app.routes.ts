04b3cac44c39a6aaf65f41ba0f3e1d132bab4046
Linh Dao <linhdh@live.com>
2024-02-20 16:39:59 +0700

    Add employee service

--- AngularHttpServiceExample1/src/app/app.routes.ts
+++ AngularHttpServiceExample1/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+    { path: 'create-employee', component: EmployeeCreateComponent },
+    { path: 'employees-list', component: EmployeeListComponent },
+    { path: 'employee-edit/:id', component: EmployeeEditComponent },
+];
