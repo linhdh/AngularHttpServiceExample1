f5f19790cd71ac2aeaa98a019399d320d18a0ee9
Linh Dao <linhdh@live.com>
2024-02-20 14:47:51 +0700

    Add employee components

---
+++ AngularHttpServiceExample1/src/app/employee-create/employee-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-create.component.html',
+  styleUrl: './employee-create.component.scss'
+})
+export class EmployeeCreateComponent {
+
+}
